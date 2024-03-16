c733bbbe0a89c4f3e635ad7fb44b7c75e5513fe5
Stephanie Livengood <sreneelive@gmail.com>
2024-03-15 22:05:53 -0400

    Add mobile functionality

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,15 @@
 import '../App.css';
+import { useState } from 'react';
 
 function Header(){
+
+    const[open, setOpen] = useState(false);
+
+    function handleOpen(){
+        let toggle = !open;
+        setOpen(toggle);
+    }
+
     return(
         <>
         <div className="navbar">
@@ -11,7 +20,19 @@ function Header(){
                 <p>Shop</p>
                 <p>Contact</p>
             </div>
+            <div className="mobilenav">
+            <i class="fa-solid fa-bars fa-2xl" onClick={handleOpen}></i>
+            </div>
         </div>
+        {open &&
+        <div className="mobilemenu">
+            <div className="closebtn" onClick={handleOpen}><p>X</p></div>
+                <p>Home</p>
+                <p>About</p>
+                <p>Shop</p>
+                <p>Contact</p>
+        </div>
+}
         </>
     )
 }
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -7,9 +7,9 @@ function Info(){
             <div className="left">
                 <h2>Wake Up To A Better Tomorrow</h2>
                 <p>SolaRise is the first app on the market which will take into account the weather when you set an alarm. Each morning a personalized messaged will display ways you can help reduce carbon emissions.</p>
-                <button>
+                <div className="leftbtn"><button>
                 <i class="fa-brands fa-apple appicon"></i>
-                    Buy In App Store</button>
+                    Buy In App Store</button></div>
             </div>
             <div className="right">
                 <div className="img"></div>
